1b7c7497a9cf5f57f9920432d26f6e64f2408cea
chattie <christuaik@gmail.com>
2023-04-06 14:20:37 -0700

    breeds posting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 //console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
 
 document.addEventListener('DOMContentLoaded', e => {
     fetch(imgUrl)
@@ -10,6 +11,18 @@ document.addEventListener('DOMContentLoaded', e => {
             let image = document.createElement('img');
             image.src = dogs[i];
             document.getElementById('dog-image-container').append(image);
+        }
+    })
+
+    fetch(breedUrl)
+    .then(res => res.json())
+    .then (data => {
+        const dogs = data.message;
+        console.log(dogs);
+        for (dog in dogs){
+            let breed = document.createElement('li');
+            breed.innerText = dog;
+            document.getElementById('dog-breeds').append(breed);
         }
     })
 })
