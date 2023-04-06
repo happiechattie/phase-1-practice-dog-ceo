6b7b87deb883ccf414a059657480d88620e6369f
chattie <christuaik@gmail.com>
2023-04-06 14:12:55 -0700

    dog images posted

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('%c HI', 'color: firebrick')
+//console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+document.addEventListener('DOMContentLoaded', e => {
+    fetch(imgUrl)
+    .then(res => res.json())
+    .then(data => {
+        const dogs = data.message;
+        for (i = 0; i < dogs.length; i++){
+            let image = document.createElement('img');
+            image.src = dogs[i];
+            document.getElementById('dog-image-container').append(image);
+        }
+    })
+})
